bf217d3431f58f3eee1312ef1cdea6a048c44039
zqmillet <zqmillet@qq.com>
2015-11-23 11:49:12 +0800

    Initialize the Interface

--- Tools.for.Shanbay/FormMain.vb
+++ Tools.for.Shanbay/FormMain.vb
@@ -1,3 +1,27 @@
 ﻿Public Class FormMain
+    Dim MenuStrip As New MenuStrip
+    Dim TabControl As New TabControl
 
+    Public Sub New()
+
+        InitializeComponent()
+
+        InitializeInterface()
+
+    End Sub
+
+    Private Sub InitializeInterface()
+        With MenuStrip
+
+        End With
+
+        With TabControl
+            .Dock = DockStyle.Fill
+        End With
+
+        With Me
+            .Controls.Add(TabControl)
+            .Controls.Add(MenuStrip)
+        End With
+    End Sub
 End Class
